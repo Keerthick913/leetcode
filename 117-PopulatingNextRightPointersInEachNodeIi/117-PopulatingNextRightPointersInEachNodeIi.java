57dfc57afc42257fb41ed8be0fb49f721980f9c7
Keerthick913 <keerthickm268@gmail.com>
2026-07-14 16:16:43 +0530

    Create 117-PopulatingNextRightPointersInEachNodeIi

---
+++ 117-PopulatingNextRightPointersInEachNodeIi/117-PopulatingNextRightPointersInEachNodeIi.java
@@ -0,0 +1,27 @@
+// Last updated: 14/07/2026, 16:16:43
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node current = root;
+        while (current != null) {
+            Node dummy = new Node(0);
+            Node tail = dummy;
+            while (current != null) {
+                if (current.left != null) {
+                    tail.next = current.left;
+                    tail = tail.next;
+                }
+                if (current.right != null) {
+                    tail.next = current.right;
+                    tail = tail.next;
+                }
+                current = current.next;
+            }
+            current = dummy.next;
+        }
+        
+        return root;
+    }
+}
