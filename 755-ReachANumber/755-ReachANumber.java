30f7747e9b25473b1116521321f18b26b6b7ad3c
Keerthick913 <keerthickm268@gmail.com>
2026-07-14 16:13:35 +0530

    Create 755-ReachANumber

---
+++ 755-ReachANumber/755-ReachANumber.java
@@ -0,0 +1,19 @@
+// Last updated: 14/07/2026, 16:13:35
+class Solution {
+    public int reachNumber(int target) {
+        int sum =0 ,steps = 0;
+        if(target ==0) return 0;
+        target = Math.abs(target);
+        while(sum< target){
+            sum+=steps;
+            steps++;
+        }
+        
+        while(((sum-target)%2!=0)){
+            sum+=steps;
+            steps++;
+        }
+        return steps-1;
+
+    }
+}
