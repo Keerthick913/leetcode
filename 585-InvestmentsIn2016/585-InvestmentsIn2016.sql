7e7e467232de63e59d563dce2e35f110e85f88fa
Keerthick913 <keerthickm268@gmail.com>
2026-07-14 16:13:42 +0530

    Create 585-InvestmentsIn2016

---
+++ 585-InvestmentsIn2016/585-InvestmentsIn2016.sql
@@ -0,0 +1,18 @@
+-- Last updated: 14/07/2026, 16:13:41
+# Write your MySQL query statement below
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    -- Condition 1: tiv_2015 must be shared with others
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND (lat, lon) IN (
+    -- Condition 2: Location must be unique
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+);
